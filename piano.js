bc456ce7de800763d87c914e7e41df57b5c9e850
Katrin _Alimova <alimova.katrin8@gmail.com>
2019-05-09 13:23:46 +0300

    DinoSite

---
+++ piano.js
@@ -0,0 +1,8 @@
+let dino=new Audio('t-rex-roar.mp3')
+let dino1=new Audio('vozglas-dinozavra.mp3')
+let dino2=new Audio('rychanie-dinozavra.mp3')
+let dino3=new Audio('korotkij-ryk-tiranozavra.mp3')
+let dino4=new Audio('golos-raptora.mp3')
+
+window.addEventListener('keydown',function(){dino.play()})
+
