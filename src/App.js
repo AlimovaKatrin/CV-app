28905340416bbce30b9c786e401f9122c3e7401f
Katrin <alimova.katrin8@gmail.com>
2021-02-24 11:56:34 +0300

    nav bar layout

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
 import ProjectPage from './pages/Projects-page/Project-page';
 import NavBar from './components/Nav-bar/Nav-bar';
+import style from './App.module.scss';
+
 function App() {
     return (
-      <>
-          <NavBar/>
-          <ProjectPage/>
-      </>
+      <div className={style.app}>
+          <div>
+              <NavBar/>
+          </div>
+          <div>
+              <ProjectPage/>
+          </div>
+      </div>
     );
 }
 
--- src/components/Nav-bar/Nav-bar.js
+++ src/components/Nav-bar/Nav-bar.js
@@ -1,8 +1,17 @@
+import style from './Nav-bar.module.scss';
 
 function NavBar( props ) {
     return (
-      <nav>
-
+      <nav className={style.navContainer}>
+          <div>
+              <button>Btm</button>
+          </div>
+          <div>
+              <button>BUTTON</button>
+          </div>
+          <div>
+              <button>KNOPKA</button>
+          </div>
       </nav>
     );
 }
