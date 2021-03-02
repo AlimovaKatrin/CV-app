96f3ad70de96feae5a596c87cbd0a0fdf286914b
Katrin <alimova.katrin8@gmail.com>
2021-03-02 13:54:00 +0300

    circle extend common style

--- src/pages/Welcome-page/Welcome-page.js
+++ src/pages/Welcome-page/Welcome-page.js
@@ -2,12 +2,10 @@ import style from './Welcome.module.scss';
 import { useEffect, useState } from 'react';
 
 function WelcomePage ( props ) {
-    let counter = 0;
     const [ scrollPosition, setScrollPosition ] = useState(window.pageYOffset);
+
     const scrollHandler = () => {
-        console.log(window.pageYOffset);
         setScrollPosition(() => window.pageYOffset);
-        console.log('scroll');
     };
 
     useEffect(() => {
@@ -17,13 +15,13 @@ function WelcomePage ( props ) {
 
     return (
         <div className={style.welcomeContainer}>
-            {scrollPosition > 0 ? <div className={style.circleOne}>
+            {scrollPosition > 0 ? <div className={style.one}>
                 My photo
             </div> : null}
-            {scrollPosition > 1000 ? <div className={style.circleTwo}>
+            {scrollPosition > 1000 ? <div className={style.two}>
                 my name is katrin
             </div> : null}
-            {scrollPosition > 2000 ? <div className={style.circleThree}>
+            {scrollPosition > 2000 ? <div className={style.three}>
                 this is my page
             </div> : null}
         </div>
