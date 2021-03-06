caa83e7384ffe36a8fc811e0ed25fcbcf3f1706e
Katrin <alimova.katrin8@gmail.com>
2021-03-06 21:46:01 +0300

    Experience page: basic view

--- src/pages/Experience-page/Experience-page.js
+++ src/pages/Experience-page/Experience-page.js
@@ -1,8 +1,25 @@
 import style from './Experience-page.module.scss';
+import experience from '../../utils/experience.json';
 
 function ExperiencePage ( props ) {
     return (
-        <div>experience</div>
+        <ul className={style.experienceList}>
+            {experience.map(( el, i ) => <li key={i}>
+                <div className={style.companyCard}>
+                    <h1>Company: {el.name}</h1>
+                    <div>
+                        Date start: {el.dateStart}
+                        Date end: {el.dateEnd}
+                    </div>
+                    <div>
+                        {el.position}
+                    </div>
+                    <div>
+                        {el.description}
+                    </div>
+                </div>
+            </li>)}
+        </ul>
     );
 }
 
