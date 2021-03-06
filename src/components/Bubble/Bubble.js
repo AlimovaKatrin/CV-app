49ff8c02c2fc353825bb61436fe88a27fb502852
Katrin <alimova.katrin8@gmail.com>
2021-03-06 17:31:01 +0300

    Welcome page: bubble fixes, imgs added; Nav: testing size for mobile

--- src/components/Bubble/Bubble.js
+++ src/components/Bubble/Bubble.js
@@ -3,6 +3,8 @@ import style from './Bubble.module.scss';
 function Bubble ( { content } ) {
     return (
         <div className={style.bubbleContainer}>
+            <img src={'/imgs/bubble_small.svg'}
+                 alt={`speech bubble with text:${content}`}/>
             <div>{content}</div>
         </div>
     );
