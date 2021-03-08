2503e5510ec3ffdfd40f77b1920fe892ad5ae22f
Katrin <alimova.katrin8@gmail.com>
2021-03-08 14:54:53 +0300

    company card

--- src/pages/Experience-page/Experience-page.js
+++ src/pages/Experience-page/Experience-page.js
@@ -1,27 +1,12 @@
 import style from './Experience-page.module.scss';
 import experience from '../../utils/experience.json';
+import Card from '../../components/CompanyCard/Card';
 
 function ExperiencePage ( props ) {
     return (
         <ul className={style.experienceList}>
             {experience.map(( el, i ) => <li key={i}>
-                <div className={style.companyCard}>
-                    <h1>Company: {el.name}</h1>
-                    <div className={style.dates}>
-                        <span>
-                            <strong>Date start:</strong> {el.dateStart}
-                        </span>
-                        <span>
-                            <strong>Date end:</strong> {el.dateEnd}
-                        </span>
-                    </div>
-                    <div>
-                       <strong>Position:</strong> {el.position}
-                    </div>
-                    <div className={style.jobDescription}>
-                        {el.description}
-                    </div>
-                </div>
+                <Card job={el}/>
             </li>)}
         </ul>
     );
