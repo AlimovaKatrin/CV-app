63bdff92e2b0b3d98b39bbdb975f34f2869dce4a
Katrin <alimova.katrin8@gmail.com>
2021-03-16 08:14:45 +0300

    contact content

--- src/pages/Contact-page/Contact-page.js
+++ src/pages/Contact-page/Contact-page.js
@@ -1,5 +1,5 @@
 import style from './Contact-page.module.scss';
-
+import { contactContent } from '../../utils/content.json';
 import Form from '../../components/Form/Form';
 
 function ContactPage ( props ) {
@@ -9,9 +9,15 @@ function ContactPage ( props ) {
                 <div className={style.linksblock}>
                     <div>
                         code related profiles
+                        {contactContent.codeNetworks.map(el=><span>
+                            {el.name}
+                        </span>)}
                     </div>
                     <div>
                         social profiles
+                        {contactContent.socialNetworks.map(el=><span>
+                            {el.name}
+                        </span>)}
                     </div>
                 </div>
                 <Form/>
